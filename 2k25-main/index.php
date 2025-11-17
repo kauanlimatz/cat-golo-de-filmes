a52ce162073ae4833266bf78a99ea7b9a296b0cb
kauan <kauanlima6547@gmail.com>
2025-11-17 18:40:11 -0300

    alteração de nome

--- 2k25-main/index.php
+++ 2k25-main/index.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['loginUser']) && $_SESSION['senhaUser'] === true) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>catágolo de filmes | Login</title>
+  <title>agenda| Login</title>
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
